8906b0650d50c8c4a58c5262d0d192a7b93e2068
Azaria Gebremichael <azariagebremichael10@gmail.com>
2020-11-12 13:23:42 +0300

    shop page padding add on App.css

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
 import { Route, Switch } from "react-router-dom";
 
-const HatsPage = () => (
-  <div>
-    <h1>Hats page</h1>
-  </div>
-);
 function App() {
   return (
     <div className="App">
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route exact path="/shop/hats" component={HatsPage} />
+        <Route exact path="/shop" component={ShopPage} />
+        {/* <Route exact path="/shop/hats" component={HatsPage} /> */}
       </Switch>
     </div>
   );
